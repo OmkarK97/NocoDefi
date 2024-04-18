4d70b1a68c15683c8296c3f3e4c46b1c76f23d71
OmkarK97 <omkar139797@gmail.com>
2024-04-18 08:28:23 +0530

    chains

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,12 @@ import React from 'react'
 import '@rainbow-me/rainbowkit/styles.css';
 import { WagmiProvider } from 'wagmi';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import {
-  base,
-  mainnet,
-  optimism,
-  polygon,
-  sepolia,
-  zora,
-} from 'wagmi/chains';
 import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-const Mumbai = {
+const Erc20Token = {
   id: 80001,
   name: 'Mumbai',
   iconUrl: 'https://chainlist.org/unknown-logo.png',
@@ -26,18 +18,18 @@ const Mumbai = {
   },
 }
 
-const Botanix = {
-  id: 3636,
-  name: 'Botanix',
-  iconUrl: 'https://chainlist.org/unknown-logo.png',
-  iconBackground: '#fff',
-  nativeCurrency: { name: 'Bitcoin', symbol: 'BTC', decimals: 18 },
-  rpcUrls: {
-    default: { http: ['https://node.botanixlabs.dev'] },
-  },
-}
+// const Botanix = {
+//   id: 3636,
+//   name: 'Botanix',
+//   iconUrl: 'https://chainlist.org/unknown-logo.png',
+//   iconBackground: '#fff',
+//   nativeCurrency: { name: 'Bitcoin', symbol: 'BTC', decimals: 18 },
+//   rpcUrls: {
+//     default: { http: ['https://node.botanixlabs.dev'] },
+//   },
+// }
 
-const Testing = {
+const IndexStrategy = {
   id: 42161,
   name: 'Testing',
   iconUrl: 'https://chainlist.org/unknown-logo.png',
@@ -52,15 +44,8 @@ const config = getDefaultConfig({
   appName: 'RainbowKit App',
   projectId: 'YOUR_PROJECT_ID',
   chains: [
-    mainnet,
-    polygon,
-    optimism,
-    base,
-    zora,
-    sepolia,
-    Mumbai,
-    Botanix,
-    Testing
+    Erc20Token,
+    IndexStrategy
   ],
   ssr: true,
 });
